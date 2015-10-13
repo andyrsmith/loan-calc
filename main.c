08e268b85d8237fb61d1e1bd2fb06882ae2036f8
Andy Smith <smith780@gmail.com>
2015-10-12 21:58:16 -0400

    Accepts more inputs
    And then prints them out.

--- main.c
+++ main.c
@@ -3,15 +3,29 @@
 int main(int argc, char *argv[]){
 
   char name[100];
-  int amount;
+  float amount;
+  float average_payment;
+  float percent;
 
   printf("---Loan Calc---\n");
 
   printf("Enter the name of the loan: ");
-  fgets(name, 100, stdin);
+  scanf("%s", name);
 
   printf("Enter the amount on the loan: ");
-  fscan(stdin, "%i", amount);
+  scanf("%f", amount);
+
+  printf("Enter the apr on the loan: ");
+  scanf("%f", &percent);
+
+  printf("Enter average monthly payment: ");
+  scanf("%f", &average_payment);
+  
+  //print out useful info
+  printf("The name of the Loan is %s\n", name);
+  printf("The amount on the loan is %f\n", amount);
+  printf("The apr on the loan is %f\n", percent);
+  printf("The average monthly payment is %f\n", average_payment);
 
   return 0;
 }
